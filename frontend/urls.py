ced6b6441ef26a6c2bc97124306cce936c0987e6
Deo <klpdeo03@gmail.com>
2022-02-12 00:03:18 +0800

    updated

--- frontend/urls.py
+++ frontend/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("login", views.perform_login, name="perform_login"),
     path("home", views.render_home, name="render_home"),
     path("logout", views.perform_logout, name="perform_logout"),
-    path("register", views.perform_register, name='perform_register'),
+    path("user-create/", views.perform_register, name='perform_register'),
     path("users", views.show_users, name='show_users'),
     # path("success", views.render_success, name='render_success'),
 ]
